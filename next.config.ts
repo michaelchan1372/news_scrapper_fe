4647ec2ec1fee73fc1048166e819ab47621fc6ab
Michael Chan <michaelchan1372@gmail.com>
2025-05-19 15:29:04 -0700

    Try fix http mix https error

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,14 @@ const nextConfig: NextConfig = {
     remote_connection: "http://18.118.2.9",
     local_connection: "http://localhost:8000"
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/scrape',
+        destination: 'http://18.118.2.9/scrape',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
