04a7cf5e8c0024664f591877cea69ddb6033740f
Michael Chan <michaelchan1372@gmail.com>
2025-05-19 17:46:34 -0700

    update into using https

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    remote_connection: "http://18.118.2.9",
+    remote_connection: "https://18.118.2.9",
     local_connection: "http://localhost:8000"
   },
   async rewrites() {
